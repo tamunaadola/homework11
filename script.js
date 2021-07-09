48b4169d82b15c23f506fe6359933d98e9c9bd11
tamunaadola <tamoadolashvili@gmail.com>
2021-07-09 15:23:08 +0400

    add primitive types and create radius of the circle

---
+++ script.js
@@ -0,0 +1,16 @@
+// TASK 2  PRIMITIVE TYPES.
+let age=24;
+let a=5.4;
+let name= "mariam";
+let x = true;
+let y=false;
+let nothing= null;
+let z= undefined;
+ 
+
+// TASK 3//
+let r = 5;
+let s = 3.14 *(r*r);
+let l= 2* (3.14*r);
+
+//TASK4//
