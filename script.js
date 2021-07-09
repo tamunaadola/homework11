b0ef1bf9951f96aa8a638c1ed90aeb9299a867fe
tamunaadola <tamoadolashvili@gmail.com>
2021-07-09 17:06:55 +0400

    add tasks

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // TASK 2  PRIMITIVE TYPES.
 let age=24;
-let a=5.4;
-let name= "mariam";
+let m =5.4;
+let name= "hello";
 let x = true;
 let y=false;
 let nothing= null;
@@ -10,7 +10,16 @@ let z= undefined;
 
 // TASK 3//
 let r = 5;
-let s = 3.14 *(r*r);
+let s = 3.14 *(r * r);
 let l= 2* (3.14*r);
 
 //TASK4//
+let a = 4;
+let b = 3;
+let c = Math.sqrt((a * a) + (b * b));
+
+//TASK5//
+let size = prompt(100);
+let v = a*a*a;
+
+
